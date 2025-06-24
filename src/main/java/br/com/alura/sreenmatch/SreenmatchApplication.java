47a979ba417b2e3687cf230ecec988f5b4894990
Pedro Cedro <phcedro84@gmail.com>
2025-06-24 18:29:37 -0300

    Inicio do projeto Spring

---
+++ src/main/java/br/com/alura/sreenmatch/SreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.alura.sreenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.alura.sreenmatch.model.DadosSerie;
+import br.com.alura.sreenmatch.service.ConsumoApi;
+import br.com.alura.sreenmatch.service.ConverteDados;
+
+@SpringBootApplication
+public class SreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=8cca9ce4");
+//		System.out.println(json);
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+
+}
